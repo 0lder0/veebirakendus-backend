d2cc61454bdd0f2eb749957906dd584894132e0f
Lilii <lilii-ann.older@voco.ee>
2023-04-03 14:29:41 +0300

    Ül 9

---
+++ index.ts
@@ -0,0 +1,31 @@
+import express, {Express, Request, Response} from "express";
+import mongoose from "mongoose";
+import ArticleController from "./controllers/articleController";
+import commentController from "./controllers/commentController";
+import authorController from "./controllers/authorController";
+
+
+const app: Express = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://liliiannolder:gwsP9HsLAVptOZxG@cluster0.hkhhyeq.mongodb.net/test");
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+app.use('/', ArticleController);
+app.use('/', commentController);
+app.use('/', authorController);
+
+app.listen(3000, () => {
+    console.log(`[server]: Server is running at http://localhost:3000`);
+});
