c65dfff6bfacde5a08e1884dcdeaa33612e01367
Lilii <lilii-ann.older@voco.ee>
2023-05-03 11:41:33 +0300

    backend

--- index.ts
+++ index.ts
@@ -1,31 +1,23 @@
-import express, {Express, Request, Response} from "express";
-import mongoose from "mongoose";
-import ArticleController from "./controllers/articleController";
-import commentController from "./controllers/commentController";
-import authorController from "./controllers/authorController";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import productsController from "./controllers/products";
+import productListController from "./controllers/productlist";
+import bodyParser from "body-parser";
 
 
 const app: Express = express();
-app.use(express.json());
-
-mongoose.connect("mongodb+srv://liliiannolder:gwsP9HsLAVptOZxG@cluster0.hkhhyeq.mongodb.net/test");
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ['http://localhost:3006']
+}));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
-app.use('/', ArticleController);
-app.use('/', commentController);
-app.use('/', authorController);
 
-app.listen(3000, () => {
+app.use('/', productsController);
+app.use('/', productListController);
+
+app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
 });
